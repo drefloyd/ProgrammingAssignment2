7d8319b96c02bc3d76756de1592d53867e499b9e
andre floyd <64279881+drefloyd@users.noreply.github.com>
2023-11-09 19:31:04 -0500

    #1 complete

--- P2/publisher.py
+++ P2/publisher.py
@@ -1,15 +1,14 @@
 import paho.mqtt.client as mqtt
-from random import randrange, uniform
-import time
+
 
 mqttBroker = "mqtt.eclipseprojects.io"  # establishes broker
-client = mqtt.Client("Temperature_inside")  # create client and name
-client.connect(mqttBroker)  # connects client to broker
+client = mqtt.Client("MQTTLock")  # create client and name
+
+client.connect(mqttBroker)  # connects to broker
+
+passwordEntered = "N/A"
 
 while True:
-    randomNumber = uniform(20.0, 21.0)  # random decimal from 20 to 21
-    client.publish("TEMPERATURE", randomNumber)  # publishes the random number on the "TEMPERATURE" topic
-    print("Just published " + str(randomNumber) + " to Topic: TEMPERATURE")
-    time.sleep(1)
-
-# from youtube video: https://www.youtube.com/watch?v=kuyCd53AOtg
+    passwordEntered = input("Enter a password to Lock/Unlock: ")
+    client.publish("MQTTLock", passwordEntered)
+    print(f"Just published '{passwordEntered}' to Topic: MQTTLock")
--- P2/subscriber.py
+++ P2/subscriber.py
@@ -1,17 +1,34 @@
 import paho.mqtt.client as mqtt
 import time
 
-def on_message(client, userdata, message):
-    print("Received message: ", str(message.payload.decode("utf-8")))
+isLocked = 0
 
+
+def on_message(client, userdata, message):
+    global isLocked
+    print("Received message: ", str(message.payload.decode("utf-8")))
+    if message.payload.decode("utf-8") == password:
+        if isLocked == 1:
+            isLocked = 0
+            print(f"correct password given, lock is now locked!")
+        else:
+            isLocked = 1
+        print(f"correct password given, lock is now unlocked!")
+    else:
+        print(f"incorrect password given!")
+
+
+password = "123abc"
 mqttBroker = "mqtt.eclipseprojects.io"
-client = mqtt.Client("Smartphone")
+client = mqtt.Client("Lock")
+
+client.on_message = on_message  # when a message is received call the function
+
 client.connect(mqttBroker)
 
 client.loop_start()
-client.subscribe("TEMPERATURE")
-
-client.on_message = on_message
+client.subscribe("MQTTLock")
 
 time.sleep(30)
-client.loop_end()
+client.loop_stop()
+client.disconnect()
